bafaf5246aa5b9580297f34f9ff2f76e760dda2a
Dario Balač <dario.balac@gmail.com>
2022-05-03 22:14:47 +0200

    Creating Table for items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
 import { ApiService } from './services/api.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +13,13 @@ import { ApiService } from './services/api.service';
 })
 export class AppComponent implements OnInit {
   title = 'Angular-Crud_App';
+
+  displayedColumns: string[] = ['itemName', 'category', 'date', 'condition', 'price', 'comment', 'action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor(private dialog: MatDialog, private api : ApiService){
 
   }
@@ -27,12 +37,23 @@ export class AppComponent implements OnInit {
     this.api.getItems()
     .subscribe({
       next:(res)=>{
-        console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       },
       error:(err)=>{
         alert("Error while fetching items.")
       }
     })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
  
