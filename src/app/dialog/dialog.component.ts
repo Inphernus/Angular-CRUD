3774295147ecaa60aeb721994ed38d8243dc1d11
Dario Balač <dario.balac@gmail.com>
2022-05-03 21:29:47 +0200

    Adding service for posting and getting items.

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, Form } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import {MatDialogRef} from '@angular/material/dialog'
 
 @Component({
   selector: 'app-dialog',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
-  constructor() { }
+  itemCondition = ["Garbage", "Used", "New"];
+  itemForm !: FormGroup;
+  constructor(private formBuilder:FormBuilder, private api : ApiService, private dialogRef : MatDialogRef<DialogComponent>) { }
 
   ngOnInit(): void {
+    this.itemForm = this.formBuilder.group({
+      itemName : ['', Validators.required],
+      category : ['', Validators.required],
+      condition : ['', Validators.required],
+      price : ['', Validators.required],
+      comment : ['', Validators.required],
+      date : ['', Validators.required],
+    })
+  }
+
+  addItem(){
+    if (this.itemForm.valid){
+      this.api.postItem(this.itemForm.value)
+      .subscribe({
+        next:(res)=> {
+          alert("Item Added!");
+          this.itemForm.reset();
+          this.dialogRef.close('save');
+        },
+        error:()=>{
+          alert("Item is not added.")
+        }
+      })
+    }
   }
 
 }
